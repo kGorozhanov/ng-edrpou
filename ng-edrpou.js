e1f57dc14e83d45e59f27f74c571a47eaff7c4a0
kostia777999 <kostya.gorozhanov@mail.ru>
2016-11-29 11:45:48 +0200

    add directive

---
+++ ng-edrpou.js
@@ -0,0 +1,35 @@
+'use strict';
+angular.module('ngEdrpou').directive('checkEdrpou', function () {
+
+    var linkFn = function (scope, elem, attrs, ngModelCtrl) {
+        var getControlValue = function (edrpou, controls) {
+            var sum = 0;
+            for (var i = 0; i < controls.length; i++) {
+                sum += +edrpou[i] * controls[i];
+            }
+            return sum % 11;
+        }
+        scope.$watch(attrs.ngModel, function (edrpou) {
+            var result = false;
+            var controlValue;
+
+            if (+edrpou && edrpou.length === 8) {
+                if (+edrpou < 30000000 || +edrpou > 60000000) {
+                    controlValue = getControlValue(edrpou, [1, 2, 3, 4, 5, 6, 7]);
+                    if (controlValue > 10) controlValue = getControlValue(edrpou, [3, 4, 5, 6, 7, 8, 9]);
+                } else {
+                    controlValue = getControlValue(edrpou, [7, 1, 2, 3, 4, 5, 6]);
+                    if (controlValue > 10) controlValue = getControlValue(edrpou, [9, 3, 4, 5, 6, 7, 8]);
+                }
+                if (controlValue === +edrpou[7]) result = true;
+            }
+
+            ngModelCtrl.$setValidity('edrpou', result);
+        });
+    };
+    return {
+        restrict: 'A',
+        link: linkFn,
+        require: 'ngModel'
+    };
+});
